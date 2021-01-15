49fd694b16033aeaf8900b774fe153e0592c88b5
Logan Blackstad <logan.blackstad@gmail.com>
2021-01-15 16:18:41 -0500

    serialize and deserialize user to create cookies

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -24,6 +24,17 @@ module.exports = function (passport) {
 };
 */
 
+passport.serializeUser((user,done)=>{
+    done(null, user.id);
+})
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then((user)=>{
+        done(null, user);
+    });
+});
+
+
 passport.use(new GoogleStrategy({
     clientID: "489821279032-k3puvqnf5e4v6612jvgi0kdsp9skgdhr.apps.googleusercontent.com",
     clientSecret: process.env.CLIENT_SECRET, // points to the .env file
@@ -37,13 +48,15 @@ passport.use(new GoogleStrategy({
         if(currentUser){
             // already have the user
             console.log('user is:', currentUser)
+            done(null, currentUser)
         } else {
             // if not, create a new user in the db
             new User({
                 username: profile.displayName,
                 googleId: profile.id
             }).save().then((newUser)=>{
-                console.log('new user created' + newUser)
+                console.log('new user created' + newUser);
+                done(null, newUser)
             })
 
         }
